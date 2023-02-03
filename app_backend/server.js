27b01d1ec62e01cce5e4722179ab57d363a97013
Aswin-S-T <aswinswebart15@gmail.com>
2023-02-03 23:44:18 +0530

    frontend and backend base created

---
+++ app_backend/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+	res.send("Node application started");
+});
+
+app.listen(port, () => {
+	console.log(`Server is running at the port ${port}`);
+});
